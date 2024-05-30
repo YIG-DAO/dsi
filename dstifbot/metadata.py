2c3afaa2caab1e7243caba46ca30ade47251e7ce
Tobalo <tobalo@yeetum.com>
2024-05-30 10:47:11 -0500

    dev: modify metadata for yeetum dsi deployment

--- dstifbot/metadata.py
+++ dstifbot/metadata.py
@@ -6,13 +6,13 @@ Information describing the project.
 
 # The package name, which is also the "UNIX name" for the project.
 package = "dstifbot"
-project = "Dragon Security Threat Intel Feed Bot"
+project = "Yeetum Dragon Security Threat Intel Feed Bot"
 project_no_spaces = project.replace(" ", "")
 version = "0.0.1"
-description = "It posts to dstif.io from various threat intel feeds"
-authors = ["Dragon Security"]
+description = "It posts to si.yeetum.com from various intel feeds"
+authors = ["Yeetum Dragon Security"]
 authors_string = ", ".join(authors)
-emails = ["support@dragonsecurity.io"]
+emails = ["info@yeetum.com"]
 license = "UNLICENSED"
 copyright = "2023 " + authors_string
-url = "https://dstif.io/"
+url = "https://yeetum.com"
